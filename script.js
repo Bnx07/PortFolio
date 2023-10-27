6aa0ec831de1f6b556f7d7e6cf1b3b7562490fd8
Bnx07 <benjabastan@gmail.com>
2023-10-26 22:25:42 -0300

    New design advances

--- script.js
+++ script.js
@@ -1,55 +1,102 @@
-let trainMargin = document.getElementById("train");
-let main = document.getElementById("main");
-let intendedPosition = 0;
-let percent = 0;
+// ! A lo mejor podría mantener la idea del tren haciendo que aparezca un > en el contenido del sidebar en el que estoy
 
-// TODO: Añadir que se mueva despacio y no a tirones
-// * A lo mejor un "transition: all ease 300ms" funca
-// ! No funciona el "transition: all ease 300ms" porque al pasar ese tiempo, tiene que acelerar el nuevo recorrido
-// * Si no, debería guardar el dato en un lugar y que se actualice frame a frame
+// TODO: Considerar si meter un chat o algo por el estilo, algo de backend
 
-window.addEventListener('scroll', (e) => {
-    let mainHeight = main.clientHeight;
-    let mainPos = main.getBoundingClientRect().top; // Va de 0 a -1088
-    let screenHeight = window.innerHeight;
+let sidebar = document.getElementById("sidebar");
+let sidebarButtons = document.getElementsByClassName("sidebar-button");
+let sidebarDivs = document.getElementsByClassName("sidebar-div");
+let main = document.getElementById('main');
 
-    let space = mainHeight - screenHeight;
-    
-    percent = mainPos / space; // ? Va desde 0 hasta -1, siendo -1 el final del todo
-    // FIXME: El 100% significa que está por debajo de la pantalla
-    
-    intendedPosition = -space * percent;
+sidebar.addEventListener('click', (event) => {
 
-    // train.style.marginTop = `${-space * percent}px`;
+    event.stopPropagation();
+
+    if (sidebar.style.transform != 'translateX(-20px)') {
+        sidebar.style.transform = 'translateX(-20px)';
+        // TODO: Añadir que se desplace el limite izquierdo del main
+        main.classList.add('movedRight');
+    } else {
+        sidebar.style.transform = 'translateX(-400px)';
+        main.classList.remove('movedRight');
+        // TODO: Añadir que se desplace el limite izquierdo del main
+    }
 })
 
-// ? Movimiento continuo con setInterval
-// * Podría usar transition: all linear 300ms para que no sea a tirones
+for (const sidebarButton of sidebarButtons) {
+    sidebarButton.addEventListener('click', (event) => {
 
-setInterval(() => {
-    console.log(parseInt(train.style.marginTop, 10))
-    if (parseInt(train.style.marginTop, 10) < intendedPosition) {
-        console.log(train.style.marginTop)
-        console.log(intendedPosition)
-        train.style.marginTop = `${intendedPosition}px`;
-        // TODO: Hacer que se mueva de a poco hacia la posición deseada (Sumar x pixeles)
-    } else if (parseInt(train.style.marginTop, 10) > intendedPosition) {
-        console.log(train.style.marginTop)
-        console.log(intendedPosition)
-        train.style.marginTop = `${intendedPosition}px`;
-    }
-}, 500);
+        event.stopPropagation();
 
-// ? Movimiento de estación a estación
-// * La idea es que hayan estaciones de cada página y al llegar a una el tren va desde la estación actual a la otra
+        let div = document.getElementById(`${sidebarButton.id}Div`);
 
-// let stations = document.getElementsByClassName('station');
+        for (const sidebarDiv of sidebarDivs) {
+            if (sidebarDiv.style.height != "0" && sidebarDiv.id != div.id) {
+                sidebarDiv.style.height = "0";
+            }
+        }
+        
+        if (div.style.height != `${div.childElementCount * 22}px`) {
+            div.style.height = `${div.childElementCount * 22}px`;
+        } else {
+            div.style.height = "0";
+        }
+    })
+};
 
-// let position = 0;
+for (const sidebarDiv of sidebarDivs) sidebarDiv.addEventListener('click', (event) => event.stopPropagation());
 
-// ! El while true revienta la página
-// while (true) {
-//     if (Math.abs(intendedPosition - parseInt(train.style.marginTop, 10)) < 100) {
-//         console.log("Cerca");
+// let trainMargin = document.getElementById("train");
+// let main = document.getElementById("main");
+// let intendedPosition = 0;
+// let percent = 0;
+
+// // TODO: Añadir que se mueva despacio y no a tirones
+// // * A lo mejor un "transition: all ease 300ms" funca
+// // ! No funciona el "transition: all ease 300ms" porque al pasar ese tiempo, tiene que acelerar el nuevo recorrido
+// // * Si no, debería guardar el dato en un lugar y que se actualice frame a frame
+
+// window.addEventListener('scroll', (e) => {
+//     let mainHeight = main.clientHeight;
+//     let mainPos = main.getBoundingClientRect().top; // Va de 0 a -1088
+//     let screenHeight = window.innerHeight;
+
+//     let space = mainHeight - screenHeight;
+    
+//     percent = mainPos / space; // ? Va desde 0 hasta -1, siendo -1 el final del todo
+//     // FIXME: El 100% significa que está por debajo de la pantalla
+    
+//     intendedPosition = -space * percent;
+
+//     // train.style.marginTop = `${-space * percent}px`;
+// })
+
+// // ? Movimiento continuo con setInterval
+// // * Podría usar transition: all linear 300ms para que no sea a tirones
+
+// setInterval(() => {
+//     console.log(parseInt(train.style.marginTop, 10))
+//     if (parseInt(train.style.marginTop, 10) < intendedPosition) {
+//         console.log(train.style.marginTop)
+//         console.log(intendedPosition)
+//         train.style.marginTop = `${intendedPosition}px`;
+//         // TODO: Hacer que se mueva de a poco hacia la posición deseada (Sumar x pixeles)
+//     } else if (parseInt(train.style.marginTop, 10) > intendedPosition) {
+//         console.log(train.style.marginTop)
+//         console.log(intendedPosition)
+//         train.style.marginTop = `${intendedPosition}px`;
 //     }
-// }
+// }, 500);
+
+// // ? Movimiento de estación a estación
+// // * La idea es que hayan estaciones de cada página y al llegar a una el tren va desde la estación actual a la otra
+
+// // let stations = document.getElementsByClassName('station');
+
+// // let position = 0;
+
+// // ! El while true revienta la página
+// // while (true) {
+// //     if (Math.abs(intendedPosition - parseInt(train.style.marginTop, 10)) < 100) {
+// //         console.log("Cerca");
+// //     }
+// // }
